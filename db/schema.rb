df86def7ef57e06cc597ef9516fe50f12c6b9dce
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-13 19:37:12 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "students", force: :cascade do |t|
+    t.integer "grade"
+    t.string  "birthdate"
   end
 
 end
