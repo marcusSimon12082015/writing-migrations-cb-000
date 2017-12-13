f5fc811ee61f038475fb66370b17dbe98d15e93d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-13 19:45:00 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeForBirthday < ActiveRecord::Migration
   def change
-    change_column(:students, :birthdate, :datetime)
+    change_column :students, :birthdate, :datetime
   end
 end
